d3b3c7191e8756bb20520dec1b4bf881ffb8fb88
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2013-06-17 22:13:52 +0900

    db access

--- anq/confirm.php
+++ anq/confirm.php
@@ -11,6 +11,36 @@ if(isset($_POST["modify"])){
 }
 
 if(isset($_POST["register"])){
+	$db = db_connect();
+
+	$name_val = $_SESSION["name"];
+	$sex_val = $_SESSION["sex"];
+	$age_val = $_SESSION["age"];
+	$animal_val = $_SESSION["animal"];
+	$comment_val = $_SESSION["comment"];
+	$del_flag = '0';
+	$create_datetime = date("Y-m-d H:i:s");
+
+	$_SESSION = array();
+
+	$sth = $db->prepare(
+		'INSERT INTO anq_t(name, sex, age, animal, 
+		comment, del_flag, create_datetime) VALUES(?,?,?,?,?,?,?)');
+	if(MDB2::isError($sth)){
+		die($sth->getMessage());	
+		exit;
+	}
+
+	$res = $sth->execute(array($name_val, $sex_val, $age_val, 
+		$animal_val, $comment_val, $del_flag, $create_datetime));
+	if(MDB2::isError($res)){
+		die($res->getMessage());	
+		print "エラーが発生しました。再度、アンケートフォームよりご登録ください<br />";
+		exit;
+	}
+
+	$smarty = new MySmarty();
+	$smarty->display("complete.tpl");
 	exit;
 }
 
